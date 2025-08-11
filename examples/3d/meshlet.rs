b8bf2c9bbf9d878fcef430fe438571601cd23d9d
SparkyPotato <noob.sparkypotato@gmail.com>
2025-08-12 00:58:39 +0100

    add test

--- examples/3d/meshlet.rs
+++ examples/3d/meshlet.rs
@@ -6,8 +6,11 @@
 mod camera_controller;
 
 use bevy::{
-    light::{CascadeShadowConfigBuilder, DirectionalLightShadowMap},
-    pbr::experimental::meshlet::{MeshletMesh3d, MeshletPlugin},
+    anti_aliasing::taa::{TemporalAntiAliasPlugin, TemporalAntiAliasing},
+    pbr::experimental::meshlet::{
+        MeshletMesh, MeshletMesh3d, MeshletPlugin,
+        MESHLET_DEFAULT_VERTEX_POSITION_QUANTIZATION_FACTOR,
+    },
     prelude::*,
     render::render_resource::AsBindGroup,
 };
@@ -24,28 +27,22 @@ fn main() -> ExitCode {
     }
 
     App::new()
-        .insert_resource(DirectionalLightShadowMap { size: 4096 })
         .add_plugins((
             DefaultPlugins,
             MeshletPlugin {
-                cluster_buffer_slots: 1 << 14,
+                cluster_buffer_slots: 1 << 25,
             },
             MaterialPlugin::<MeshletDebugMaterial>::default(),
             CameraControllerPlugin,
         ))
         .add_systems(Startup, setup)
+        .add_systems(Update, plop_meshes)
         .run();
 
     ExitCode::SUCCESS
 }
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut standard_materials: ResMut<Assets<StandardMaterial>>,
-    mut debug_materials: ResMut<Assets<MeshletDebugMaterial>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         Camera3d::default(),
         Transform::from_translation(Vec3::new(1.8, 0.4, -0.1)).looking_at(Vec3::ZERO, Vec3::Y),
@@ -62,15 +59,9 @@ fn setup(
     commands.spawn((
         DirectionalLight {
             illuminance: light_consts::lux::FULL_DAYLIGHT,
-            shadows_enabled: true,
+            shadows_enabled: false,
             ..default()
         },
-        CascadeShadowConfigBuilder {
-            num_cascades: 1,
-            maximum_distance: 15.0,
-            ..default()
-        }
-        .build(),
         Transform::from_rotation(Quat::from_euler(EulerRot::ZYX, 0.0, PI * -0.15, PI * -0.15)),
     ));
 
@@ -78,48 +69,71 @@ fn setup(
     // that has been converted to a [`bevy_pbr::meshlet::MeshletMesh`]
     // using [`bevy_pbr::meshlet::MeshletMesh::from_mesh`], which is
     // a function only available when the `meshlet_processor` cargo feature is enabled.
-    let meshlet_mesh_handle = asset_server.load("external/models/bunny.meshlet_mesh");
-    let debug_material = debug_materials.add(MeshletDebugMaterial::default());
+    let dragon = asset_server.load("external/models/dragon.gltf#Mesh0/Primitive0");
+    commands.insert_resource(Dummy(Some(dragon)));
+}
 
-    for x in -2..=2 {
-        commands.spawn((
-            MeshletMesh3d(meshlet_mesh_handle.clone()),
-            MeshMaterial3d(standard_materials.add(StandardMaterial {
-                base_color: match x {
-                    -2 => Srgba::hex("#dc2626").unwrap().into(),
-                    -1 => Srgba::hex("#ea580c").unwrap().into(),
-                    0 => Srgba::hex("#facc15").unwrap().into(),
-                    1 => Srgba::hex("#16a34a").unwrap().into(),
-                    2 => Srgba::hex("#0284c7").unwrap().into(),
+#[derive(Resource)]
+struct Dummy(Option<Handle<Mesh>>);
+
+fn plop_meshes(
+    mut commands: Commands,
+    mut run: ResMut<Dummy>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut m_meshes: ResMut<Assets<MeshletMesh>>,
+    mut standard_materials: ResMut<Assets<StandardMaterial>>,
+    mut debug_materials: ResMut<Assets<MeshletDebugMaterial>>,
+) {
+    if let Some(mesh) = run.0.as_ref()
+        && let Some(mesh) = meshes.get(mesh)
+    {
+        run.0 = None;
+        let mesh =
+            MeshletMesh::from_mesh(mesh, MESHLET_DEFAULT_VERTEX_POSITION_QUANTIZATION_FACTOR)
+                .unwrap();
+        let handle = m_meshes.add(mesh);
+        let debug_material = debug_materials.add(MeshletDebugMaterial::default());
+
+        let mats: [_; 5] = std::array::from_fn(|i| {
+            standard_materials.add(StandardMaterial {
+                base_color: match i {
+                    0 => Srgba::hex("#dc2626").unwrap().into(),
+                    1 => Srgba::hex("#ea580c").unwrap().into(),
+                    2 => Srgba::hex("#facc15").unwrap().into(),
+                    3 => Srgba::hex("#16a34a").unwrap().into(),
+                    4 => Srgba::hex("#0284c7").unwrap().into(),
                     _ => unreachable!(),
                 },
-                perceptual_roughness: (x + 2) as f32 / 4.0,
                 ..default()
-            })),
-            Transform::default()
-                .with_scale(Vec3::splat(0.2))
-                .with_translation(Vec3::new(x as f32 / 2.0, 0.0, -0.3)),
-        ));
-    }
-    for x in -2..=2 {
-        commands.spawn((
-            MeshletMesh3d(meshlet_mesh_handle.clone()),
-            MeshMaterial3d(debug_material.clone()),
-            Transform::default()
-                .with_scale(Vec3::splat(0.2))
-                .with_rotation(Quat::from_rotation_y(PI))
-                .with_translation(Vec3::new(x as f32 / 2.0, 0.0, 0.3)),
-        ));
-    }
+            })
+        });
 
-    commands.spawn((
-        Mesh3d(meshes.add(Plane3d::default().mesh().size(5.0, 5.0))),
-        MeshMaterial3d(standard_materials.add(StandardMaterial {
-            base_color: Color::WHITE,
-            perceptual_roughness: 1.0,
-            ..default()
-        })),
-    ));
+        for x in -25i32..=25 {
+            for y in -25i32..=25 {
+                for z in -25i32..=25 {
+                    if (x & 1) == 0 {
+                        commands.spawn((
+                            MeshletMesh3d(handle.clone()),
+                            MeshMaterial3d(debug_material.clone()),
+                            Transform::default()
+                                .with_translation(Vec3::new(x as f32, y as f32, z as f32))
+                                .with_rotation(Quat::from_rotation_x(PI / 2.0))
+                                .with_scale(Vec3::splat(3.0)),
+                        ));
+                    } else {
+                        commands.spawn((
+                            MeshletMesh3d(handle.clone()),
+                            MeshMaterial3d(mats[(z.abs() % 5) as usize].clone()),
+                            Transform::default()
+                                .with_translation(Vec3::new(x as f32, y as f32, z as f32))
+                                .with_rotation(Quat::from_rotation_x(PI / 2.0))
+                                .with_scale(Vec3::splat(3.0)),
+                        ));
+                    }
+                }
+            }
+        }
+    }
 }
 
 #[derive(Asset, TypePath, AsBindGroup, Clone, Default)]
